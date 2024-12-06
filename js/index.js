bf5aa9d90236ee2c176671fdd115c3b91c12d643
Morshedul Islam <nilrat10@gamil.com>
2024-12-06 22:28:21 +0600

    some code is added

--- js/index.js
+++ js/index.js
@@ -2,7 +2,14 @@
 const donateButton = document.getElementById('donate-button');
 donateButton.addEventListener('click', function(){
 
+
     const donateAmount = parseFloat(document.getElementById('donate-amount').value);
+
+    if (donateAmount <= 0 || isNaN(donateAmount)) {
+        alert("Please enter a correct amount!");
+        return;
+    }
+
     
    const totalDonateAmount = document.getElementById('total-donate-amount');
    let currentAmount = parseInt(totalDonateAmount.textContent); 
@@ -10,6 +17,14 @@ donateButton.addEventListener('click', function(){
    let newAmount = donateAmount + currentAmount;
 
    totalDonateAmount.innerHTML = `<img src="assets/coin.png">${newAmount}<span>BDT</span>`;
+
+
+    const mainBalance = document.getElementById('my-main-balance');
+    let presentAmount = parseInt(mainBalance.textContent);
+    let restAmount = presentAmount - donateAmount;
+    mainBalance.innerHTML = `<img src="assets/coin.png">${restAmount}<span>BDT</span>`;
+
+    
 })
 
 
